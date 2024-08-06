564d89de5b18f498dcfbc117c2cf38ae9b1d1987
Student User <student@workstation.lab.example.com>
2024-08-06 04:44:46 -0400

    changed

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Hello, Google ! Matrix Reloaded Version is " . PHP_VERSION . "\n";
+print "Hello, Google ! Matrix REVOLUTION Version is " . PHP_VERSION . "\n";
 ?>
