0b2fb64e47d8101a5bbe75eace4eee5ca718137b
Student User <student@workstation.lab.example.com>
2025-03-04 03:23:59 -0500

    changed index.html

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Hello, Neo The matrix has you Matrix Version " . PHP_VERSION . "\n";
+print "Hello, The inception has you! you are in a dream of Version " . PHP_VERSION . "\n";
 ?>
